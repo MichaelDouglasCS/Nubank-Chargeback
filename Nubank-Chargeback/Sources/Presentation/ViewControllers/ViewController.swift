b4102b15929a37b0a1c498b392eeaadc1ea98adb
Michael Douglas <michael.dcsouza@gmail.com>
2017-09-15 01:04:35 -0300

    initial commit of full repository

---
+++ Nubank-Chargeback/Sources/Presentation/ViewControllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nubank-Chargeback
+//
+//  Created by Michael Douglas on 14/09/17.
+//  Copyright © 2017 MichaelDouglas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Nubank-Chargeback' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Nubank-Chargeback
+
+  target 'Nubank-ChargebackTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Nubank-ChargebackUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
